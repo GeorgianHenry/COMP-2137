4cb0f65bf70e11955cd95b231e4af77904e5c882
Henry Picanco <200529162@student.georgianc.on.ca>
2023-08-15 17:46:02 -0400

    Final version (most likely).

--- NMSconfigauto.sh
+++ NMSconfigauto.sh
@@ -30,12 +30,11 @@ configureHost() {
     local newIP="$3"
 
     # SSH into the target system. --KeyCheck false so no inputs required  
-    if ssh -o StrictHostKeyChecking=no "$target" << EOF
-        echo "Configuring $newHostname" # Outputs for user, cannot use printOutput in this case since we are in a target
+    ssh -o StrictHostKeyChecking=no "$target" << EOF
+        echo "Configuring $newHostname" 
         
         # Update package information and redirect quietly
         sudo apt-get update > /dev/null
-
 
         # Check and change the hostname if needed
         if [[ \$(hostname) != "$newHostname" ]]; then
@@ -47,33 +46,27 @@ configureHost() {
             echo "Hostname was set correctly already."
         fi
 
-
         # Set new IP address
         echo "Changing IP to $newIP"
         sudo ip addr add "$newIP/24" dev eth0
         [ \$? -eq 0 ] && echo "IP was set."
-
 
         # Add hostname to /etc/hosts
         echo "Adding $newHostname to /etc/hosts"
         echo "192.168.16.4 $newHostname" | sudo tee -a /etc/hosts
         [ \$? -eq 0 ] && echo "Added $newHostname."
 
-
         # Install UFW if not installed
         echo "Installing UFW if not found"
         sudo apt-get install -y ufw > /dev/null
-
 
         # Enable UFW firewall and force the rules
         echo "Enabling UFW firewall"
         sudo ufw --force enable
 
-
         # Apply firewall rules
         echo "Applying firewall rules"
         sudo ufw allow 22/tcp
-
 
         # Restart rsyslog for logging and uncomment lines
         echo "Restarting rsyslog"
@@ -82,12 +75,11 @@ configureHost() {
         sudo systemctl restart rsyslog
         [ \$? -ne 0 ] && { echo "Failed to restart rsyslog. Exiting."; exit 1; }
 
-
         # Check if rsyslog is running
         sudo systemctl is-active -q rsyslog && echo "Rsyslog is running on $newHostname" || { echo "Rsyslog is not running on $newHostname. Exiting."; exit 1; }
 
 EOF
-    then
+    if [ $? -eq 0 ]; then
         printOutput "$newHostname configuration complete, no errors."
     else
         printOutput "$newHostname configuration failed, exiting code 1."
@@ -95,14 +87,11 @@ EOF
     fi
 }
 
-
 # Main script starts here, our functions are being executed
 configureSSH
 
-# Target 1 configuration function, will call it then puts the vars in $ variables
 configureHost "remoteadmin@172.16.1.10" "loghost" "192.168.16.3"
 
-# Target 2 configuration same idea these will run one at a time though and takes time to run
 configureHost "remoteadmin@172.16.1.11" "webhost" "192.168.16.4"
 
 # NMS configuration
@@ -122,7 +111,7 @@ else
     apt-get install -y curl > /dev/null || { printOutput "Curl install failed. Exiting."; exit 1; }
 fi
 
-#  Checking for the webhost page
+# Checking for the webhost page
 if curl -s "http://webhost" | grep -q "Apache2 Default Page"; then
     printOutput "Found webhost page on http://webhost"
 else
