e5938dab080daacbd437b21dbd34f163739aa9d0
Henry Picanco <200529162@student.georgianc.on.ca>
2023-08-15 15:30:31 -0400

    Readability changes and tweaks

--- NMSconfigauto.sh
+++ NMSconfigauto.sh
@@ -2,6 +2,7 @@
 # Automated Configuration Script
 # Author: Henry Picanco, 200529162@student.georgianc.on.ca
 # Date: 2023-08-07
+# Updated: 2023-08-15
 
 # Function to print messages with timestamp
 printOutput() {
@@ -14,7 +15,7 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 fi
 
-# Function to configure SSH
+# Function to install the SSH
 configureSSH() {
     printOutput "Checking pre-requisites for SSH configuration. . ."
     apt update -y
@@ -28,14 +29,15 @@ configureHost() {
     local newHostname="$2"
     local newIP="$3"
 
-    # SSH into the target system
-    if ssh -o StrictHostKeyChecking=no "$target" << EOF
-        echo "Configuring $newHostname"
+    # SSH into the target system. --KeyCheck false so no inputs required  
+    if ssh -o StrictHostKeyChecking= no "$target" << EOF
+        echo "Configuring $newHostname" # Outputs for user, cannot use printOutput in this case since we are "in" a target
         
-        # Update package information
+        # Update package information and redirect quietly
         sudo apt-get update > /dev/null
 
-        # Check and change hostname if needed
+
+        # Check and change the hostname if needed
         if [[ \$(hostname) != "$newHostname" ]]; then
             echo "Changing hostname to $newHostname"
             echo "$newHostname" | sudo tee /etc/hostname
@@ -45,34 +47,41 @@ configureHost() {
             echo "Hostname was set correctly already."
         fi
 
+
         # Set new IP address
         echo "Changing IP to $newIP"
         sudo ip addr add "$newIP/24" dev eth0
         [ \$? -eq 0 ] && echo "IP was set."
+
 
         # Add hostname to /etc/hosts
         echo "Adding $newHostname to /etc/hosts"
         echo "192.168.16.4 $newHostname" | sudo tee -a /etc/hosts
         [ \$? -eq 0 ] && echo "Added $newHostname."
 
+
         # Install UFW if not installed
         echo "Installing UFW if not found"
         sudo apt-get install -y ufw > /dev/null
 
-        # Enable UFW firewall
+
+        # Enable UFW firewall and force the rules
         echo "Enabling UFW firewall"
         sudo ufw --force enable
+
 
         # Apply firewall rules
         echo "Applying firewall rules"
         sudo ufw allow 22/tcp
 
-        # Restart rsyslog for logging
+
+        # Restart rsyslog for logging and uncomment lines
         echo "Restarting rsyslog"
         sudo sed -i 's/#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
         sudo sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
         sudo systemctl restart rsyslog
         [ \$? -ne 0 ] && { echo "Failed to restart rsyslog. Exiting."; exit 1; }
+
 
         # Check if rsyslog is running
         sudo systemctl is-active -q rsyslog && echo "Rsyslog is running on $newHostname" || { echo "Rsyslog is not running on $newHostname. Exiting."; exit 1; }
@@ -86,7 +95,8 @@ EOF
     fi
 }
 
-# Main script starts here
+
+# Main script starts here, our functions are being executed
 configureSSH
 
 # Target 1 configuration function, will call it then puts the vars in $ variables
@@ -95,7 +105,7 @@ configureHost "remoteadmin@172.16.1.10" "loghost" "192.168.16.3"
 # Target 2 configuration same idea these will run one at a time though and takes time to run
 configureHost "remoteadmin@172.16.1.11" "webhost" "192.168.16.4"
 
-# NMS (Network Management System) configuration
+# NMS configuration
 # Remove previous loghost and webhost entries from /etc/hosts
 sed -i '/\(loghost\|webhost\)/d' /etc/hosts
 # Add loghost entry to /etc/hosts
@@ -112,13 +122,13 @@ else
     apt-get install -y curl > /dev/null || { printOutput "Curl install failed. Exiting."; exit 1; }
 fi
 
-#  Checking accessibility of the webhost page
+#  Checking for the webhost page
 if curl -s "http://webhost" | grep -q "Apache2 Default Page"; then
     printOutput "Found webhost page on http://webhost"
 else
-    printOutput "Failed to find webhost page on http://webhost"
+    printOutput "Failed to find webhost page on http://webhost. Was there a previous error?"
 fi
 
-# Display completion message
+# Display completion message where script ends
 printOutput "Automated configuration process has been completed."
 
